79ceb01405f3139292e11caf002ef5d55f53f93b
namrataukirde <namrata.ukirde@kiprosh.com>
2020-03-16 02:51:06 +0530

    check user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,17 @@ class User < ApplicationRecord
   has_many :projects, through: :users_projects
 
   validates :first_name, :last_name, presence: true
+
+  def role_names
+    roles.pluck(:name)
+  end
+
+  def admin?
+    role_names.include?('admin')
+  end
+
+  def developer?
+    role_names.include?('developer')
+  end
 end
 
